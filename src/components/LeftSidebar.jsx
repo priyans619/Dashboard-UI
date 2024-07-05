14127d19628b586a73cbd3a5ec383ae62669cec3
priyans619 <priyansh.2b@gmail.com>
2024-07-05 23:24:10 +0530

    add sidebar component

---
+++ src/components/LeftSidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const LeftSidebar = () => {
+  
+
+  return (
+    <aside className="w-1/6 bg-gray-500 text-white p-4">
+      <h1 className="text-2xl mb-4">PAGE</h1>
+    </aside>
+  );
+};
+
+export default LeftSidebar;
