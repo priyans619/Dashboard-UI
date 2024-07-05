f2c6c41c723913ffe1a7162fca8f8e3a73efc028
priyans619 <priyansh.2b@gmail.com>
2024-07-05 23:21:45 +0530

    add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <nav className="bg-white p-4 shadow-md">
+      <h1 className="text-xl">Navbar</h1>
+    </nav>
+  );
+};
+
+export default Header;
