326aa6a3c549880d3186aaa58bdfa2eda765299f
priyans619 <priyansh.2b@gmail.com>
2024-07-05 23:44:05 +0530

    card layout

--- src/pages/overview/Overview.jsx
+++ src/pages/overview/Overview.jsx
@@ -4,6 +4,30 @@ const Overview = () => {
   return (
     <div className="p-4">
       <h2 className="text-2xl mb-4">Overview Page</h2>
+      <div className="flex flex-wrap gap-8 justify-center lg:justify-start">
+        
+        <div className="bg-white p-6 w-52 h-28 rounded-lg shadow-md flex-shrink-0">
+          <h3 className="text-xl">Card 1</h3>
+          <p className="text-gray-600">Content 1</p>
+        </div>
+        
+        <div className="bg-white p-6 w-52 h-28 rounded-lg shadow-md flex-shrink-0">
+          <h3 className="text-xl">Card 2</h3>
+          <p className="text-gray-600">Content 2</p>
+        </div>
+        
+        <div className="bg-white p-6 w-52 h-28 rounded-lg shadow-md flex-shrink-0">
+          <h3 className="text-xl">Card 3</h3>
+          <p className="text-gray-600">Content 3</p>
+        </div>
+        
+        <div className="bg-white p-6 w-52 h-28 rounded-lg shadow-md flex-shrink-0">
+          <h3 className="text-xl">Card 4</h3>
+          <p className="text-gray-600">Content 4</p>
+        </div>
+      </div>
+      
+
     </div>
   );
 };
