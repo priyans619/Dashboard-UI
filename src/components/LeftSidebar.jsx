1013fa5977a7c666f82586f407666c394642db92
priyans619 <priyansh.2b@gmail.com>
2024-07-05 23:33:54 +0530

    setting toggle using state hook, link to overview

--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const LeftSidebar = () => {
+  const [isOverviewOpen, setIsOverviewOpen] = useState(false);
+
+  const toggleOverview = () => {
+    setIsOverviewOpen(!isOverviewOpen);
+  };
 
   return (
     <aside className="w-1/6 bg-blue-500 text-white p-4">
@@ -9,13 +14,19 @@ const LeftSidebar = () => {
       <nav>
         <ul>
           <li className="mb-2">
-            
+            <div className="flex items-center">
+              <button onClick={toggleOverview} className="focus:outline-none">
+                {isOverviewOpen ? '<' : '>'}
+              </button>
+              <Link to="/" className="ml-2">Overview</Link>
+            </div>
+            {isOverviewOpen && (
               <ul className="ml-4 mt-2">
                 <li className="mb-2"><Link to="/overview/page1">Page 1</Link></li>
                 <li className="mb-2"><Link to="/overview/page2">Page 2</Link></li>
                 <li className="mb-2"><Link to="/overview/page3">Page 3</Link></li>
               </ul>
-            
+            )}
           </li>
           
         </ul>
