154eafcbed95799b93aebe50e2fa5752ba30767b
priyans619 <priyansh.2b@gmail.com>
2024-07-05 23:42:02 +0530

    add page for overview section

---
+++ src/pages/overview/Overview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Overview = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl mb-4">Overview Page</h2>
+    </div>
+  );
+};
+
+export default Overview;
