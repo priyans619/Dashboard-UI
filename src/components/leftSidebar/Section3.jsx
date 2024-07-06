5ed3cbc6d648a4b7036ee85221acdc45701092bb
priyans619 <priyansh.2b@gmail.com>
2024-07-06 23:21:07 +0530

    text-UI

--- src/components/leftSidebar/Section3.jsx
+++ src/components/leftSidebar/Section3.jsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 const Section3 = () => {
   return (
     <>
-      <h1 className=" mb-4">Pages</h1>
+      <h1 className="text-sm mb-4 text-gray-400">Pages</h1>
       <nav>
         <ul>
-          <li className="mb-2"><Link to="/">Documentations</Link></li>
-          <li className="mb-2"><Link to="/">Help Center</Link></li>
+          <li className="text-sm mb-2"><Link to="/">Documentations</Link></li>
+          <li className="text-sm mb-2"><Link to="/">Help Center</Link></li>
         </ul>
       </nav>
     </>
