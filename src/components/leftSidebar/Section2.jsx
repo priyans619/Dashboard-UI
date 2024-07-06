f2e87dca4d36aa6c133ecdc2724973e246230739
priyans619 <priyansh.2b@gmail.com>
2024-07-06 23:10:14 +0530

    add section2 comp, Pages

---
+++ src/components/leftSidebar/Section2.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Section2 = () => {
+  
+  return (
+    <>
+      <h1 className="text-sm mb-2 mt-4 text-gray-400">Pages</h1>
+      <nav>
+        
+      </nav>
+    </>
+  );
+};
+
+export default Section2;
