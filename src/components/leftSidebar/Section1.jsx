1763495d462aad230f501401688b4589b24ace8f
priyans619 <priyansh.2b@gmail.com>
2024-07-06 21:43:35 +0530

    add section comp

---
+++ src/components/leftSidebar/Section1.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Section1 = () => {
+  return (
+    <>
+      <h1 className="text-2xl mb-4">Recents</h1>
+      <nav>
+        <ul>
+          
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Section1;
