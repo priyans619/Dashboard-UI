7cd35f8536f8753e767f1a096120c8efdd2c8288
priyans619 <priyansh.2b@gmail.com>
2024-07-06 23:11:24 +0530

    toggle functionlity for expansion

--- src/components/leftSidebar/Section2.jsx
+++ src/components/leftSidebar/Section2.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Section2 = () => {
+  ;
+
+const toggleSection = (section) => {
+    setOpenSections({
+      ...openSections,
+      [section]: !openSections[section]
+    });
+  };
   
   return (
     <>
       <h1 className="text-sm mb-2 mt-4 text-gray-400">Pages</h1>
       <nav>
-        
+        <ul className=''>
+          <li className="mb-2">
+            <div className="flex items-center">
+              <button onClick={() => toggleSection('overview')} className="focus:outline-none">
+                {openSections.overview ? '<' : '>'}
+              </button>
+              <Link to="/overview" className="ml-2 text-sm">Overview</Link>
+            </div>
+            
+          </li>
+        </ul>
       </nav>
     </>
   );
