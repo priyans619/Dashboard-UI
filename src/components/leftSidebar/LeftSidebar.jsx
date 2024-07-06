34a7433ba7aa8161535138d1a19e45552d35a4bd
priyans619 <priyansh.2b@gmail.com>
2024-07-06 23:25:51 +0530

    add logo and title

--- src/components/leftSidebar/LeftSidebar.jsx
+++ src/components/leftSidebar/LeftSidebar.jsx
@@ -9,6 +9,10 @@ const LeftSidebar = () => {
     <>
       {/* Desktop Sidebar */}
       <div className="md:flex hidden flex-col w-[240px] h-screen py-10 px-6 bg-[#F7F9FB]">
+        <div className="flex items-center mb-6">
+          <img src="src/assets/logo.png" alt="logo" className="w-6" />
+          <h1 className="ml-2 text-black font-normal text-sm ">Superstars AI</h1>
+        </div>
         <Section1 />
         <Section2 />
         <Section3 />
