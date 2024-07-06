ede2c88612578cfacfa2fb3b45c88bcdfe1aa856
priyans619 <priyansh.2b@gmail.com>
2024-07-06 22:21:02 +0530

    link to sub-page

--- src/components/leftSidebar/Section1.jsx
+++ src/components/leftSidebar/Section1.jsx
@@ -4,10 +4,18 @@ import { Link } from 'react-router-dom';
 const Section1 = () => {
   return (
     <>
-      <h1 className="text-2xl mb-4">Recents</h1>
+      <div className="flex gap-4 items-center text-gray-400 mb-4">
+        <h1 className="text-sm">Favorites</h1>
+        <h1 className="text-sm">Recently</h1>
+      </div>
       <nav>
         <ul>
-          
+          <li className="flex items-center mb-2">
+            <Link to="/" className="text-sm font-normal leading-5 text-center ">Get Started</Link>
+          </li>
+          <li className="flex items-center mb-2">
+            <Link to="/" className="text-sm font-normal leading-5 text-center">Transactions</Link>
+          </li>
         </ul>
       </nav>
     </>
