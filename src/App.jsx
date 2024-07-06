d959c33f280325022ea4c8404166d3d0cd7df4ef
priyans619 <priyansh.2b@gmail.com>
2024-07-06 23:32:33 +0530

    render of header and LeftSidebar comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Header from './components/Header';
+import LeftSidebar from './components/leftSidebar/LeftSidebar';
+import Overview from './pages/overview/Overview';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <div className="flex h-screen bg-gray-100">
+        <LeftSidebar />
+        
+        <main className="flex-1 p-4">
+        <Header />
+          <div className="mt-4">
+            <Routes>
+              <Route exact path="/overview" element={<Overview/>} />
+            </Routes>
+          </div>
+        </main>
+
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
