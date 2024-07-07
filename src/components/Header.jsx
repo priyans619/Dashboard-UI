d4b5c90cb7e171ee9ace58158430fdf136728a96
priyans619 <priyansh.2b@gmail.com>
2024-07-08 00:34:05 +0530

    Responsive touch to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { PiRocketLaunch, PiSidebar } from "react-icons/pi";
+import { PiRocketLaunch } from "react-icons/pi";
 import { AiTwotoneStar } from "react-icons/ai";
 import { CiLight } from "react-icons/ci";
 import { MdHistory } from "react-icons/md";
 import { FiBell } from "react-icons/fi";
-
 import { FiSearch } from 'react-icons/fi';
 
 const Header = () => {
@@ -14,7 +13,7 @@ const Header = () => {
       <div className="flex items-center space-x-4">
         <PiRocketLaunch className="text-gray-600 text-xl cursor-pointer" />
         <AiTwotoneStar className="text-gray-600 text-xl cursor-pointer" />
-        <span className="flex items-center space-x-2 cursor-pointer">
+        <span className="flex items-center space-x-1 cursor-pointer">
           <span className="text-black hover:text-gray-300">Pages</span>
           <span className="text-black">/</span>
           <span className="text-black hover:text-gray-300">Overview</span>
@@ -22,22 +21,30 @@ const Header = () => {
       </div>
 
       {/* Right side search bar and icons */}
-      <div className="flex items-center space-x-4">
+      <div className="hidden md:flex items-center space-x-4 ml-2">
         <div className="relative">
           <FiSearch className="absolute left-3 top-2 text-gray-600" />
           <input
             type="text"
-            className="text-center pl-10 pr-10 py-2 border bg-gray-200 border-gray-300 rounded-full focus:outline-none focus:border-gray-500"
+            className="hidden md:block text-center pl-10 pr-10 py-2 border bg-gray-200 border-gray-300 rounded-full focus:outline-none focus:border-gray-500"
             placeholder="Search"
             style={{ borderRadius: '8px', padding: '4px 8px', }}
           />
-
-          <img src="src/assets/icon.png" alt="logo" className="absolute right-3 top-2 w-4 h-4" />
+          <img src="src/assets/icon.png" alt="logo" className="hidden md:block absolute right-3 top-2 w-4 h-4" />
         </div>
-        <CiLight className="text-gray-600 text-xl cursor-pointer" />
-        <MdHistory className="text-gray-600 text-xl cursor-pointer" />
-        <FiBell className="text-gray-600 text-xl cursor-pointer" />
-        <PiSidebar className="text-gray-600 text-xl cursor-pointer" />
+      </div>
+
+      {/* Separate div for each icon */}
+      <div className="flex items-center space-x-4 ml-2 mr-9">
+        <div className="text-gray-600 text-xl cursor-pointer">
+          <CiLight />
+        </div>
+        <div className="text-gray-600 text-xl cursor-pointer">
+          <MdHistory />
+        </div>
+        <div className="text-gray-600 text-xl cursor-pointer">
+          <FiBell />
+        </div>
       </div>
     </nav>
   );
