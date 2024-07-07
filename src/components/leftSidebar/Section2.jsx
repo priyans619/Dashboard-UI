1097f30d261d80064427eae45959cba8f2caa46b
priyans619 <priyansh.2b@gmail.com>
2024-07-07 21:21:11 +0530

    MdKeyboardRight,MdKeyboardLeft

--- src/components/leftSidebar/Section2.jsx
+++ src/components/leftSidebar/Section2.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { MdKeyboardArrowRight, MdKeyboardArrowDown } from "react-icons/md";
 
 const Section2 = () => {
   const [openSections, setOpenSections] = useState({
@@ -20,7 +21,7 @@ const Section2 = () => {
       [section]: !openSections[section]
     });
   };
-  
+
   return (
     <>
       <h1 className="text-sm mb-2 mt-4 text-gray-400">Pages</h1>
@@ -29,13 +30,13 @@ const Section2 = () => {
           <li className="mb-2">
             <div className="flex items-center">
               <button onClick={() => toggleSection('overview')} className="focus:outline-none">
-                {openSections.overview ? '<' : '>'}
+                {openSections.overview ? <MdKeyboardArrowDown /> : <MdKeyboardArrowRight />}
               </button>
               <Link to="/overview" className="ml-2 text-sm">Overview</Link>
             </div>
             {openSections.overview && (
               <ul className="text-sm ml-4 mt-2">
-                
+                {/* Add additional links or content here if needed */}
               </ul>
             )}
           </li>
@@ -43,16 +44,19 @@ const Section2 = () => {
           <li className="mb-2">
             <div className="flex items-center">
               <button onClick={() => toggleSection('transactions')} className="focus:outline-none">
-                {openSections.transactions ? '<' : '>'}
+                {openSections.transactions ? <MdKeyboardArrowDown /> : <MdKeyboardArrowRight />}
               </button>
               <Link to="/transactions" className="ml-2 text-sm">Transactions</Link>
             </div>
             {openSections.transactions && (
               <ul className="text-sm ml-4 mt-2">
                 <li className="text-sm mb-2"><Link to="/transactions/page1">Page</Link></li>
+                {/* Add additional links or content here if needed */}
               </ul>
             )}
           </li>
+          
+          {/* Add additional sections as needed */}
         </ul>
       </nav>
     </>
