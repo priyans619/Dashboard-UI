5a110882d71c9eafa2ecc3ad2aba82a0d8f3c97a
priyans619 <priyansh.2b@gmail.com>
2024-07-07 23:27:04 +0530

    render every component in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import LeftSidebar from './components/leftSidebar/LeftSidebar';
 import RightSidebar from './components/RightSidebar';
 import Overview from './pages/overview/Overview';
-
+import Transactions from './pages/Transactions';
+import Invoices from './pages/Invoices';
 
 function App() {
   return (
     <Router>
       <div className="flex h-screen bg-gray-100">
         <LeftSidebar />
-        
-        <main className="flex-1 p-4 md:ml-[240px]">
-        <Header />
-          <div className="mt-4">
+
+        <main className="flex-1 md:ml-[240px] bg-[#F7F9FB]">
+          <Header />
+          <div className="">
             <Routes>
-              <Route exact path="/overview" element={<Overview/>} />
+              <Route exact path="/overview" element={<Overview />} />
+              <Route path="/transactions" element={<Transactions />} />
+              <Route path="/invoices" element={<Invoices />} />
             </Routes>
           </div>
         </main>
 
+        <RightSidebar />
       </div>
     </Router>
   );
