c26014e99d28a3fb444377abc89d0c4f79cf04db
priyans619 <priyansh.2b@gmail.com>
2024-07-07 19:08:51 +0530

    add notification section

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
+import { BiBugAlt } from "react-icons/bi";
 
 const RightSidebar = () => {
 
   return (
     <>
       {/* Desktop Sidebar */}
-      <div className="md:flex hidden flex-col w-[240px] h-screen py-10 px-6 bg-green-200">
-        <h1 className="text-xl mb-4">Notifications</h1>
-        <ul>
-          <li className="mb-2">Notification 1</li>
-          <li className="mb-2">Notification 2</li>
-        </ul>
-        <h1 className="text-xl mt-4 mb-4">Activity</h1>
-        <ul>
-          <li className="mb-2">Activity 1</li>
-          <li className="mb-2">Activity 2</li>
-        </ul>
-      </div>
+      <div className="md:flex hidden flex-col w-[240px] h-screen py-10 px-2 border-l border-gray-300 bg-green-200">
+        <h1 className="text-sm font-semibold ml-2">Notifications</h1>
+        <div className="flex items-center mb-2">
+          <BiBugAlt className="w-8 h-7 text-sm py-1 rounded bg-[#E3F5FF]" />
+          <div className="flex flex-col ml-3 mt-3">
+            <span className="text-sm text-black font-normal">You have an issue that nee..</span>
+            <span className="text-xs text-gray-400">Just now</span>
+          </div>
+        </div>
 
-      
+      </div>
     </>
   );
 };
 
 export default RightSidebar;
+
 
