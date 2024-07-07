3362cae074a201109eada6185b9e27ad8e192dca
priyans619 <priyansh.2b@gmail.com>
2024-07-07 23:25:18 +0530

    logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { PiRocketLaunch, PiSidebar  } from "react-icons/pi";
+import { AiTwotoneStar } from "react-icons/ai";
+
 
 const Header = () => {
   return (
-    <nav className="bg-[#F7F9FB] p-4 border-b border-gray-300">
+    <nav className="bg-[#F7F9FB] p-4 border-b border-gray-300 flex justify-between items-center">
+      {/* Left side icons */}
+      <div className="flex items-center space-x-4">
+        <PiRocketLaunch className="text-gray-600 text-xl cursor-pointer" />
+        <AiTwotoneStar className="text-gray-600 text-xl cursor-pointer" />
+        <span className="flex items-center space-x-2 cursor-pointer">
+          <span className="text-black hover:text-gray-300">Pages</span>
+          <span className="text-black">/</span>
+          <span className="text-black hover:text-gray-300">Overview</span>
+        </span>
+      </div>
+
       
     </nav>
   );
