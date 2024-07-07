661dcb6af2e7209a568af80363209582e94044f9
priyans619 <priyansh.2b@gmail.com>
2024-07-07 21:50:13 +0530

    functionality for change in bg when section is opened

--- src/components/leftSidebar/Section2.jsx
+++ src/components/leftSidebar/Section2.jsx
@@ -17,11 +17,17 @@ const Section2 = () => {
     developerTools: false
   });
 
+  const [activeLink, setActiveLink] = useState('');
+
   const toggleSection = (section) => {
     setOpenSections({
       ...openSections,
       [section]: !openSections[section]
     });
+  };
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
   };
 
   return (
@@ -30,27 +36,38 @@ const Section2 = () => {
       <nav>
         <ul className=''>
           <li className="mb-2">
-            <div className="flex items-center bg-gray-200 rounded-md p-1">
+            <div className={`flex items-center rounded-md p-1 ${activeLink === 'overview' ? 'bg-gray-200' : ''}`}>
               <button onClick={() => toggleSection('overview')} className="focus:outline-none">
                 {openSections.overview ? <MdKeyboardArrowDown /> : <MdKeyboardArrowRight />}
               </button>
               <PiRocketLaunch className="text-gray-600 text-xl ml-2" />
-              <Link to="/overview" className="ml-2 text-sm">Overview</Link>
+              <Link 
+                to="/overview" 
+                className="ml-2 text-sm" 
+                onClick={() => handleLinkClick('overview')}
+              >
+                Overview
+              </Link>
             </div>
             {openSections.overview && (
               <ul className="text-sm ml-4 mt-2">
-
               </ul>
             )}
           </li>
 
           <li className="mb-2">
-            <div className="flex items-center bg-gray-200 rounded-md p-1">
+            <div className={`flex items-center rounded-md p-1 ${activeLink === 'transactions' ? 'bg-gray-200' : ''}`}>
               <button onClick={() => toggleSection('transactions')} className="focus:outline-none">
                 {openSections.transactions ? <MdKeyboardArrowDown /> : <MdKeyboardArrowRight />}
               </button>
               <CiDollar className="text-gray-600 text-xl ml-2" />
-              <Link to="/transactions" className="ml-2 text-sm">Transactions</Link>
+              <Link 
+                to="/transactions" 
+                className="ml-2 text-sm" 
+                onClick={() => handleLinkClick('transactions')}
+              >
+                Transactions
+              </Link>
             </div>
             {openSections.transactions && (
               <ul className="text-sm ml-4 mt-2">
