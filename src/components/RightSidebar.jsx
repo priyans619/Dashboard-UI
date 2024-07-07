60f450a52856a9204924826a48775169e4c17e56
priyans619 <priyansh.2b@gmail.com>
2024-07-07 15:05:50 +0530

    Update rightSide bar

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -1,24 +1,27 @@
-// src/components/RightSidebar.js
-
 import React from 'react';
 
 const RightSidebar = () => {
+
   return (
-    <div className="w-1/6 bg-gray-200 p-4">
-      <h1 className="text-xl mb-4">Notifications</h1>
-      <ul>
-        <li className="mb-2">Notification 1</li>
-        <li className="mb-2">Notification 2</li>
-        {/* Add more notifications as needed */}
-      </ul>
-      <h1 className="text-xl mt-4 mb-4">Activity</h1>
-      <ul>
-        <li className="mb-2">Activity 1</li>
-        <li className="mb-2">Activity 2</li>
-        {/* Add more activities as needed */}
-      </ul>
-    </div>
+    <>
+      {/* Desktop Sidebar */}
+      <div className="md:flex hidden flex-col w-[240px] h-screen py-10 px-6 bg-green-200">
+        <h1 className="text-xl mb-4">Notifications</h1>
+        <ul>
+          <li className="mb-2">Notification 1</li>
+          <li className="mb-2">Notification 2</li>
+        </ul>
+        <h1 className="text-xl mt-4 mb-4">Activity</h1>
+        <ul>
+          <li className="mb-2">Activity 1</li>
+          <li className="mb-2">Activity 2</li>
+        </ul>
+      </div>
+
+      
+    </>
   );
 };
 
 export default RightSidebar;
+
