d1fc8f21dffb0fc7c9e07d9bbeb6f3a22928f8c2
priyans619 <priyansh.2b@gmail.com>
2024-07-07 15:02:10 +0530

    md:ml-[240px], when ham menu is open this breakpoint will move that much pixel to left

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from './components/Header';
 import LeftSidebar from './components/leftSidebar/LeftSidebar';
+import RightSidebar from './components/RightSidebar';
 import Overview from './pages/overview/Overview';
+
 
 function App() {
   return (
@@ -10,7 +12,7 @@ function App() {
       <div className="flex h-screen bg-gray-100">
         <LeftSidebar />
         
-        <main className="flex-1 p-4">
+        <main className="flex-1 p-4 md:ml-[240px]">
         <Header />
           <div className="mt-4">
             <Routes>
