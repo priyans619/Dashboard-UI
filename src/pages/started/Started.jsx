d36a9922cb442a00fb43d27962ffc439422695e7
priyans619 <priyansh.2b@gmail.com>
2024-07-08 15:27:39 +0530

    fixed title for home route

--- src/pages/started/Started.jsx
+++ src/pages/started/Started.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Started = () => {
   return (
     <div className="p-4">
-      <h2 className="text-2xl text-center">Welcome to My Dashboard</h2>
+      <h2 className="text-2xl w-3/4 text-center">Welcome to My Dashboard</h2>
     </div>
   )
 }
